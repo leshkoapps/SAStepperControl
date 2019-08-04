3c4f0a677260e2468cfd91185c7fc0d65c23dadb
Artem Meleshko <>
2019-08-05 00:37:50 +0300

    fixes

--- Classes/SAStepperControl.h
+++ Classes/SAStepperControl.h
@@ -6,8 +6,16 @@
 //  Copyright (c) 2014 SA. All rights reserved.
 //
 
-@import UIKit;
+#import <UIKit/UIKit.h>
+
+
+@class SAStepperControl;
+
+
+typedef void (^SAStepperControlActionBlock)(SAStepperControl *control);
 
 @interface SAStepperControl : UIStepper
+
+@property (nonatomic,copy)SAStepperControlActionBlock actionBlock;
 
 @end
--- Classes/SAStepperControl.m
+++ Classes/SAStepperControl.m
@@ -7,93 +7,57 @@
 //
 
 #import "SAStepperControl.h"
-#import "SAStepperControl+Image.h"
 
 @interface SAStepperControl ()
 
-@property (nonatomic, strong) UILabel * __nonnull label;
+@property (nonatomic, strong) UILabel *label;
 
 @end
 
 @implementation SAStepperControl
 
-#pragma mark -
-#pragma mark - Init
 
-- (__nonnull instancetype)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
-    
-    if (self) {
-        [self setupLabel];
+- (instancetype)initWithCoder:(NSCoder *)aDecoder{
+    self = [super initWithCoder:aDecoder];
+    if(self){
+        [self customInit];
     }
-    
     return self;
 }
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    
-    [self setupLabel];
+- (instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if(self){
+        [self customInit];
+    }
+    return self;
 }
 
-#pragma mark -
-#pragma mark - Layout
-
-- (void)layoutSubviews {
-    [super layoutSubviews];
-    
-    self.label.frame = CGRectMake(0,
-                                  0,
-                                  30,
-                                  CGRectGetHeight(self.frame));
-}
-
-#pragma mark -
-#pragma mark - Setup
-
-- (void)setupLabel {
+- (void)customInit{
     self.label = [[UILabel alloc] initWithFrame:CGRectMake(0,
                                                            0,
                                                            30,
                                                            CGRectGetHeight(self.frame))];
-    
     self.label.textAlignment = NSTextAlignmentCenter;
     self.label.textColor = self.tintColor;
     self.label.adjustsFontSizeToFitWidth = YES;
-    
-    [self setDividerImage:[self imageFromView:self.label]
-      forLeftSegmentState:UIControlStateNormal
-        rightSegmentState:UIControlStateNormal];
+    [self setDividerImage:[self imageFromView:self.label] forLeftSegmentState:UIControlStateNormal rightSegmentState:UIControlStateNormal];
+    [self addTarget:self action:@selector(actionValueChanged:) forControlEvents:UIControlEventValueChanged];
 }
 
-#pragma mark -
-#pragma mark - Tint Color
-
-- (void)tintColorDidChange {
-    [super tintColorDidChange];
-    self.label.textColor = self.tintColor;
-
-    [self setDividerImage:[self imageFromView:self.label]
-      forLeftSegmentState:UIControlStateNormal
-        rightSegmentState:UIControlStateNormal];
+- (void)actionValueChanged:(id)sender{
+    __weak typeof (self) weakSelf = self;
+    if(self.actionBlock){
+        self.actionBlock(weakSelf);
+    }
 }
-
-#pragma mark -
-#pragma mark - Override
 
 - (UIImage *)dividerImageForLeftSegmentState:(UIControlState)leftState rightSegmentState:(UIControlState)rightState {
-    return [self dividerImage];
-}
-
-#pragma mark -
-#pragma mark - Private
-
-- (UIImage * __nonnull)dividerImage {
-    if (self.value == 0) {
+    if (fabs(self.value) < FLT_EPSILON ) {
         UIView *dividerContainer = [[UIView alloc] initWithFrame:CGRectMake(14,
-                                                                            0,
-                                                                            2,
-                                                                            29)];
+                                                                   0,
+                                                                   2,
+                                                                   29)];
         
         dividerContainer.alpha = 0.5;
         dividerContainer.backgroundColor = self.tintColor;
@@ -109,13 +73,30 @@
         return [self imageFromView:self.label];
     }
     
+    
     for (UIView *view in self.label.subviews) {
         [view removeFromSuperview];
+        
     }
     
-    self.label.text = @(self.value).stringValue;
-    
+    self.label.text = [NSNumber numberWithDouble:self.value].stringValue;
+
     return [self imageFromView:self.label];
+}
+
+- (void)tintColorDidChange {
+    [super tintColorDidChange];
+    self.label.textColor = self.tintColor;
+    [self setDividerImage:[self imageFromView:self.label] forLeftSegmentState:UIControlStateNormal rightSegmentState:UIControlStateNormal];
+}
+
+- (UIImage *)imageFromView:(UIView *)view {
+    UIGraphicsBeginImageContextWithOptions(view.frame.size,NO,[UIScreen mainScreen].scale);
+    [view.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    return viewImage;
 }
 
 @end
